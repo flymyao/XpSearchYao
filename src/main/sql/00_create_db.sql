9afff27f21eb9f8bf5889a5c61ed2fa1e76e97c9
flymyao <flymyao@gmail.com>
2013-05-07 15:47:30 +0800

    .add sql for create db and seed data

---
+++ src/main/sql/00_create_db.sql
@@ -0,0 +1,15 @@
+-- Database: xpsearchyao_db
+CREATE DATABASE xpsearchyao_db
+  WITH OWNER = postgres
+       ENCODING = 'UTF8'
+       TABLESPACE = pg_default
+       CONNECTION LIMIT = -1;
+
+
+       
+-- Schema: xpsearchyao_schema
+
+CREATE SCHEMA xpsearchyao_schema
+  AUTHORIZATION postgres;
+
+
