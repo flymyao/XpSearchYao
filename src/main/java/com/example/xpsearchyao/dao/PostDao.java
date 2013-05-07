959f393ad8f3e27e98cb98313844ebd33dbdcc48
flymyao <flymyao@gmail.com>
2013-05-07 10:15:48 +0800

    .add fts for post title

--- src/main/java/com/example/xpsearchyao/dao/PostDao.java
+++ src/main/java/com/example/xpsearchyao/dao/PostDao.java
@@ -13,16 +13,22 @@ public class PostDao extends BaseHibernateDao<Post>{
 
 	public List<Map> fts(String query){
 		StringBuffer sql = new StringBuffer();
-		sql.append("select title,ts_headline(body, plainto_tsquery('")
+		sql.append("select ")
+		.append("ts_headline(title, plainto_tsquery('")
+		.append(query).append("'))  as title ")
+		.append(",ts_headline(body, plainto_tsquery('")
 		.append(query).append("'))  as body ")
 		.append(",ts_rank(to_tsvector(body), plainto_tsquery('")
 		.append(query)
-		.append("')) as rank ")
+		.append("')) as bodyRank ")
+		.append(",ts_rank(to_tsvector(title), plainto_tsquery('")
+		.append(query)
+		.append("')) as titleRank ")
 		.append(" from xpsearchyao_schema.post ")
 		.append(" where ts_rank(to_tsvector(body), plainto_tsquery('")
 		.append(query)
 		.append(" '))>0 ")
-		.append(" order by rank desc");
+		.append(" order by titleRank,bodyRank desc");
 		return daoHelper.getSession().createSQLQuery(sql.toString())
 		.setResultTransformer(Transformers.ALIAS_TO_ENTITY_MAP).list();
 	}
