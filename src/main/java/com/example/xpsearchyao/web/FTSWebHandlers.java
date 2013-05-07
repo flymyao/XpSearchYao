69098bc1a462569e9393634bbe03fdd7c8a54891
flymyao <flymyao@gmail.com>
2013-05-07 14:35:09 +0800

    .add DbConnectionManager to get db Connection

--- src/main/java/com/example/xpsearchyao/web/FTSWebHandlers.java
+++ src/main/java/com/example/xpsearchyao/web/FTSWebHandlers.java
@@ -6,6 +6,7 @@ import com.britesnow.snow.web.handler.annotation.WebModelHandler;
 import com.britesnow.snow.web.param.annotation.WebModel;
 import com.britesnow.snow.web.param.annotation.WebParam;
 import com.example.xpsearchyao.dao.PostDao;
+import com.example.xpsearchyao.hook.DbConnectionManager;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 
@@ -15,8 +16,12 @@ public class FTSWebHandlers {
 	@Inject
 	private PostDao postDao;
 	
+	@Inject
+	private DbConnectionManager dbConnectionManager;
+	
 	@WebModelHandler(startsWith="/search")
 	public void search(@WebModel Map m, @WebParam("keywords")String keywords){
+		System.out.println(dbConnectionManager.getConnection());
 		m.put("results", postDao.fts(keywords));
 		m.put("keywords", keywords);
 	}
