8bed78b58ffd6e1ae390ab3887b7de061d472292
flymyao <flymyao@gmail.com>
2013-05-10 18:08:33 +0800

    .create comment table and add seed data

--- src/main/java/com/example/xpsearchyao/web/DataWebHandler.java
+++ src/main/java/com/example/xpsearchyao/web/DataWebHandler.java
@@ -25,26 +25,39 @@ public class DataWebHandler {
 	public Map<String,Object> importData(@PathVar("name")Table table) throws SQLException{
 		Map<String,Object> result = new HashMap<String,Object>();
 		StringBuffer sql = new StringBuffer();
-		sql.append("insert into xpsearchyao_schema.").append(table)
-		.append(" (id,acceptedanswerid,answercount,body,commentcount,communityowneddate,")
-		.append("creationdate,favoritecount,lasteditoruserid,owneruserid,posttypeid,score,tag,title,viewcount)")
-		.append(" values ");
-		for(Map m:DataReader.readXML(table.name())){
-			sql.append("(").append(m.get("id")).append(",")
-				.append(m.get("acceptedanswerid")).append(",")
-				.append(m.get("answercount")).append(",")
-				.append(getString(m.get("body"))).append(",")
-				.append(m.get("commentcount")).append(",")
-				.append(getDateString(m.get("communityowneddate"))).append(",")
-				.append(getDateString(m.get("creationdate"))).append(",")
-				.append(m.get("favoritecount")).append(",")
-				.append(m.get("lasteditoruserid")).append(",")
-				.append(m.get("owneruserid")).append(",")
-				.append(m.get("posttypeid")).append(",")
-				.append(m.get("score")).append(",")
-				.append(getString(m.get("tag"))).append(",")
-				.append(getString(m.get("title"))).append(",")
-				.append(m.get("viewcount")).append("),");
+		if(table.equals(Table.Post)){
+			sql.append("insert into xpsearchyao_schema.").append(table)
+			.append(" (id,acceptedanswerid,answercount,body,commentcount,communityowneddate,")
+			.append("creationdate,favoritecount,lasteditoruserid,owneruserid,posttypeid,score,tag,title,viewcount)")
+			.append(" values ");
+			for(Map m:DataReader.readXML(table.name())){
+				sql.append("(").append(m.get("id")).append(",")
+					.append(m.get("acceptedanswerid")).append(",")
+					.append(m.get("answercount")).append(",")
+					.append(getString(m.get("body"))).append(",")
+					.append(m.get("commentcount")).append(",")
+					.append(getDateString(m.get("communityowneddate"))).append(",")
+					.append(getDateString(m.get("creationdate"))).append(",")
+					.append(m.get("favoritecount")).append(",")
+					.append(m.get("lasteditoruserid")).append(",")
+					.append(m.get("owneruserid")).append(",")
+					.append(m.get("posttypeid")).append(",")
+					.append(m.get("score")).append(",")
+					.append(getString(m.get("tag"))).append(",")
+					.append(getString(m.get("title"))).append(",")
+					.append(m.get("viewcount")).append("),");
+			}
+		}else if(table.equals(Table.Comment)){
+			sql.append("insert into xpsearchyao_schema.").append(table)
+			.append(" (id,postid,text,creationdate,userid)")
+			.append(" values ");
+			for(Map m:DataReader.readXML(table.name())){
+				sql.append("(").append(m.get("id")).append(",")
+					.append(m.get("postid")).append(",")
+					.append(getString(m.get("text"))).append(",")
+					.append(getDateString(m.get("creationdate"))).append(",")
+					.append(m.get("userid")).append("),");
+			}
 		}
 		PreparedStatement statement = dbConnectionManager.getConnection().prepareStatement(sql.substring(0,sql.length()-1));
 		statement.execute();
