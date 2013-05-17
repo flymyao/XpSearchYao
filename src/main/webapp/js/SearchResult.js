45385aa1cd55b0b63167e7c145e6a3d96b06bb03
flymyao <flymyao@gmail.com>
2013-05-17 18:28:23 +0800

    .fix the search issue to apply brite view

--- src/main/webapp/js/SearchResult.js
+++ src/main/webapp/js/SearchResult.js
@@ -4,7 +4,42 @@
 			console.log(data);
 			return app.render("tmpl-SearchResult",{data:data});
 		},
-		events:{}
+		events:{
+			"btap;.btn":function(event){
+				var $btn = $(event.target);
+				var type = $btn.attr("data-turn");
+				var $div = $btn.closest(".resultsPage");
+				var pg = $div.attr("data-pg");
+				var keywords = $div.attr("data-keywords");
+				if(type=='prev'){
+					pg = (+pg)-1;
+					if(pg<1){
+						pg = 1;
+						alert("this is the first page");
+						return false;
+					}
+				}else{
+					pg=(+pg)+1;
+				}
+				
+
+				$.ajax({
+					url:'/search',
+					type:'Get',
+					data:{
+						keywords:keywords,
+						type:'default',
+						pg:pg
+					},
+					dataType:"json",
+					success:function(data){
+						brite.display("SearchResult","",data);
+					}
+				});
+				
+			
+			}
+		}
 	});
 
 })();
